d0926ae6650d4e41ddab672d0bdc90fa325cbac9
NAD777 <nehaev07.07.2003@gmail.com>
2024-10-20 21:20:02 +0300

    Add public api

--- source/src/BNFC/Backend/Swift/CFToSwiftBuilder.hs
+++ source/src/BNFC/Backend/Swift/CFToSwiftBuilder.hs
@@ -71,15 +71,6 @@ mkBuildTokenFunction tokenCat = vcat
       "Double"  -> "Double(ctx.getText()!)!"
       _         -> "ctx.getText()!"
 
--- | generate name for function which will build node for some cat.
-mkBuildFnName :: Cat -> String
-mkBuildFnName cat = "build" ++ firstUpperCase (restName cat)
-  where
-    restName cat = case cat of
-      ListCat cat  -> restName cat ++ "List"
-      TokenCat cat -> cat ++ "Token"
-      otherCat     -> catToStr otherCat
-
 mkBuildFunction :: String -> RuleData -> Doc
 mkBuildFunction lang (cat, rulesWithLabels)  = vcat
     [ text $ "func" +++ mkBuildFnName cat ++ "(_ ctx: " ++ (addParserPrefix lang $ identCat cat) ++ "Context) throws ->" +++ catToSwiftType cat +++ "{"
